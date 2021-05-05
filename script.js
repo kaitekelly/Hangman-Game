3dceaf2bb390cf924a54655200c790b76fa5aa36
kaitekelly <murphy.kaite@gmail.com>
2021-05-05 16:37:47 -0700

    hangman javascript logic

--- script.js
+++ script.js
@@ -36,4 +36,75 @@ function displayWord() {
     }
 }
 
+//Update the wrong letters
+function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = `
+    ${wrongLettersEl.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+    // Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    });
+
+    //Check if lost
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost. 😕';
+        popup.style.display = 'flex';
+    }
+}
+
+//Show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+       notification.classList.remove('show'); 
+    }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+    // console.log(e.keyCode);
+    if(e.keyCode >= 65 && e.keyCode <=90) {
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+    }
+})
+
+//Event listener to restart game
+playAgainBtn.addEventListener('click', () => {
+    //Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    displayWord();
+
+    updateWrongLettersEl();
+    popup.style.display = 'none';
+})
+
 displayWord();
